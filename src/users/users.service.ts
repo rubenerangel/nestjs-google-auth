172337ddff8b62d4d3f05ca0063d73ec84807a2d
Rubén Rangel <rubenrang@gmail.com>
2022-06-16 11:36:56 -0500

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [
+    {
+      id: 0,
+      name: 'Bob',
+      email: 'bob@gmail.com',
+      password: 'bobPass',
+    },
+
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'johnPass',
+    },
+
+    {
+      id: 2,
+      name: 'Gary',
+      email: 'gary@gmail.com',
+      password: 'garyPass',
+    },
+  ];
+
+  findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find((user: User) => user.email === email);
+
+    if (user) {
+      return Promise.resolve(user);
+    }
+
+    return undefined;
+  }
+
+  findOne(id: number): Promise<User | undefined> {
+    const user = this.users.find((user: User) => user.id === id);
+
+    if (user) {
+      return Promise.resolve(user);
+    }
+
+    return undefined;
+  }
+}
